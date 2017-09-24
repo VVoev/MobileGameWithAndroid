ad77d5a2db8db3931b326ee27acae5734ad03a6f
VVoev <vladovoev@yahoo.co.uk>
2017-09-24 14:38:14 +0300

    added game loop

---
+++ IcyTower/app/src/main/java/me/icytower/Game/MainThread.java
@@ -0,0 +1,95 @@
+package me.icytower.Game;
+
+import android.graphics.Canvas;
+import android.provider.Settings;
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread {
+
+    private static final int ONE_MILLION = 1_000_000;
+    private static final int MAX_FPS = 30;
+    private double averageFPS;
+
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePannel;
+
+    private boolean running;
+    public static Canvas canvas;
+
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel){
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePannel = gamePanel;
+    }
+
+    //Override default sleep since its deprecated in thread
+    public void wait(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(){
+        long startTime;
+        long timeMiliseconds = 1000/MAX_FPS;
+        long waitTime;
+        int frameCount = 0;
+        long totalTime = 0;
+        long targetTime =1000/MAX_FPS;
+
+        while (running){
+            startTime = System.nanoTime();
+            canvas = null;
+
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder){
+                    this.gamePannel.update();
+                    this.gamePannel.draw(canvas);
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                if(canvas != null){
+                    try{
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+            timeMiliseconds = (System.nanoTime()-startTime)/ONE_MILLION;
+            waitTime = targetTime - timeMiliseconds;
+            try {
+                if(waitTime>0){
+                    this.wait(waitTime);
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if(frameCount == MAX_FPS){
+                double operation = (totalTime/frameCount)/ONE_MILLION;
+                averageFPS = 1000 /operation;
+                frameCount = 0;
+                totalTime = 0;
+                System.out.print(averageFPS);
+            }
+        }
+    }
+
+
+}
--- IcyTower/app/src/main/java/me/icytower/NewGame.java
+++ IcyTower/app/src/main/java/me/icytower/NewGame.java
@@ -1,15 +1,23 @@
 package me.icytower;
 
+import android.app.Activity;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class NewGame extends AppCompatActivity {
+import me.icytower.Game.GamePanel;
+
+public class NewGame extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_game);
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState,persistentState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GamePanel(this));
         
     }
 }
