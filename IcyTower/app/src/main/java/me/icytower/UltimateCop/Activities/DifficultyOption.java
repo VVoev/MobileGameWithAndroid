cb8dfbd1ca4dcde81ba1624e90d8464d2d186a66
VVoev <vladovoev@yahoo.co.uk>
2017-10-11 22:31:34 +0300

    Project Done

--- IcyTower/app/src/main/java/me/icytower/UltimateCop/Activities/DifficultyOption.java
+++ IcyTower/app/src/main/java/me/icytower/UltimateCop/Activities/DifficultyOption.java
@@ -2,12 +2,8 @@ package me.icytower.UltimateCop.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 import me.icytower.R;
 import me.icytower.UltimateCop.GlobalConstants.Constants;
